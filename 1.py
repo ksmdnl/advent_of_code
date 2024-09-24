9939bcc20beb819ad9c22c85848110433420a5dd
Daniel Kusuma <daniel.gabrielyudha@gmail.com>
2024-09-24 16:46:42 +0200

    first problem

---
+++ 1.py
@@ -0,0 +1,19 @@
+import re
+
+digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+entry = "heightseven4two5"
+
+def match():
+    pass
+
+def main():
+    first_digit = None
+    second_digit = None
+
+    length = len(entry) // 2
+
+    for i in range(length):
+
+
+if __name__ == "__main__":
+    main()
